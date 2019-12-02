00d3773bc6b5ce032ce3873db9c413e7750c6d83
EugSh <eug.shkalev@yandex.ru>
2019-12-02 18:51:50 +0300

    commit1

---
+++ src/test/java/java/core/Header.java
@@ -0,0 +1,14 @@
+package java.core;
+
+import org.openqa.selenium.WebDriver;
+
+public class Header extends BasePage {
+    public Header(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    protected void check() {
+
+    }
+}
---
+++ src/test/java/java/core/LoginPage.java
@@ -0,0 +1,14 @@
+package java.core;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+    public LoginPage(final WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    protected void check() {
+
+    }
+}
