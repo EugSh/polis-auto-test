123ac5f325af690bdea1d0607bc8348104bee2bd
blaackpanter <pranowa.l@yandex.ru>
2019-12-14 17:13:00 +0300

    header: friends, notifications B1: search friend, add B2: check invite, agree

--- src/test/java/project/core/Header.java
+++ src/test/java/project/core/Header.java
@@ -1,6 +1,11 @@
 package project.core;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 public class Header extends BasePage {
     public Header(WebDriver driver) {
@@ -9,6 +14,80 @@ public class Header extends BasePage {
 
     @Override
     protected void check() {
+        assertTrue(explicitWait(ExpectedConditions.presenceOfElementLocated(By.className("toolbar")),
+                10,
+                1000));
+        assertTrue(explicitWait(ExpectedConditions.presenceOfElementLocated(By.className("it")),
+                10,
+                1000));
+        assertTrue(explicitWait(ExpectedConditions.presenceOfElementLocated(By.className("toolbar_search_lupa")),
+                10,
+                1000));
+        assertTrue(explicitWait(ExpectedConditions.presenceOfElementLocated(By.className("toolbar_nav_i_ic")),
+                10,
+                1000));
+        assertTrue(explicitWait(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[contains(@data-l,'t,friends')]")),
+                10,
+                1000));
 
     }
+
+    public PostInvite doSearch(String loginBot) {
+        type(loginBot, By.className("it"));
+        click(By.className("toolbar_search_lupa"));
+        PostInvite postInvite = new PostInvite();
+        postInvite.check();
+        return postInvite;
+    }
+
+    public Notifications checkNotifications() {
+        click(By.xpath(".//*[contains(@data-l,'t,notifications')]"));
+        Notifications notifications = new Notifications();
+        notifications.check();
+        return notifications;
+    }
+
+    public class Notifications {
+        public Notifications() {
+        }
+
+        private void check() {
+            assertTrue(explicitWait(ExpectedConditions.presenceOfElementLocated(By.className("notif_tx")),
+                    10,
+                    1000));
+            assertTrue(explicitWait(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[contains(@data-l,'t,btn_accept')]")),
+                    10,
+                    1000));
+
+        }
+
+        public boolean agree() {
+            click(By.xpath(".//*[contains(@data-l,'t,btn_accept')]"));
+            return driver.findElement(By.xpath("//div[contains(text(), 'и вы теперь друзья')]")).isDisplayed();
+        }
+    }
+
+    public class PostInvite {
+        public PostInvite() {
+        }
+
+        private void check() {
+            /*assertTrue(explicitWait(ExpectedConditions.presenceOfElementLocated(By.className("gs_result_list")),
+                    10,
+                    1000));
+            assertTrue(explicitWait(ExpectedConditions.presenceOfElementLocated(By.className("hookBlock")),
+                    10,
+                    1000));*/
+            assertTrue(explicitWait(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(text(), 'Добавить в друзья')]")),
+                    10,
+                    1000));
+
+        }
+
+        public boolean addFriend() {
+            click(By.xpath("//span[contains(text(), 'Добавить в друзья')]"));
+            return driver.findElement(By.xpath("//span[contains(text(), 'Запрос отправлен')]")).isDisplayed();
+        }
+    }
 }
+
